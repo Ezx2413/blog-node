96f3d5f9398adcb0415287feeeedcf3fa405b1e3
ezx24 <87664754+Ezx2413@users.noreply.github.com>
2022-07-27 14:33:52 +0430

    base project

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`);
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
